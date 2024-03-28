5300a6675335f51d922725c9b1e2b96938ee72da
LeandroAmaralC <leandrocorreia35@icloud.com>
2024-03-28 12:23:39 -0300

    GUARDA DE ROTAS

--- gatitobook/src/app/app-routing.module.ts
+++ gatitobook/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+import { LoginGuard } from './autenticacao/login.guard';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', loadChildren:()=> import('./home/home.module').then((m) =>m.HomeModule),
+  canLoad: [LoginGuard]
    },
    {path: 'animais',
     loadChildren: ()=> import('./animais/animais.module').then((m) => m.AnimaisModule),
+    canLoad: [AutenticacaoGuard],
   },
 ];
 
